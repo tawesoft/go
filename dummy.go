d59c639634bd511a563fa05a04eaa5656d5726c6
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-06 13:40:42 +0000

    Update docs

--- dummy.go
+++
@@ -1,6 +0,0 @@
-/*
-Package Go is a dummy package for a monorepo of Go modules from Tawesoft.
-
-See https://tawesoft.co.uk/go for subdirectories.
-*/
-package dummy
---
+++ tawesoft.go
@@ -0,0 +1,4 @@
+/*
+Package tawesoft is a base package for a monorepo of Go modules from Tawesoft.
+*/
+package tawesoft
