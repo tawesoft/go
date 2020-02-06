cca757e0fc3b48aa394afda55b9a019c47c4f937
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-06 13:29:43 +0000

    Update doc

---
+++ humanize/huamnize.go
@@ -0,0 +1,13 @@
+/*
+Package humanize implements lightweight human-readable numbers for Go.
+
+The API is incomplete and may be subject to occasional breaking changes.
+
+Home page https://tawesoft.co.uk/go
+
+For source code see https://github.com/tawesoft/go/master/humanize
+
+For documentation see https://godoc.org/tawesoft.co.uk/go/humanize
+
+*/
+package humanize
