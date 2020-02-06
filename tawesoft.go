285aff7e399f66d979a553deda660b3993e9face
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-06 14:05:53 +0000

    Update docstring

--- tawesoft.go
+++ tawesoft.go
@@ -1,4 +1,8 @@
 /*
 Package tawesoft is a base package for a monorepo of Go modules from Tawesoft.
+
+For a summary, source, links, news, license information, support etc. please
+see https://www.tawesoft.co.uk/go
+
 */
 package tawesoft
