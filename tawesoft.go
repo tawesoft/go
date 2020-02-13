a1370a09e367b20b34ad9207c6f9d12b639cca99
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-13 18:08:36 +0000

    Update global import

--- tawesoft.go
+++ tawesoft.go
@@ -11,6 +11,7 @@ import (
     _ "tawesoft.co.uk/go/dialog"
     _ "tawesoft.co.uk/go/glcaps"
     _ "tawesoft.co.uk/go/humanize"
+    _ "tawesoft.co.uk/go/operator"
     _ "tawesoft.co.uk/go/ximage"
     _ "tawesoft.co.uk/go/ximage/xcolor"
 )
