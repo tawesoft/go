083135b4f5552e114ce99c3b061a83c6d2805d2d
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-14 19:38:45 +0000

    Update docstring

--- operator/operator.go
+++ operator/operator.go
@@ -1,3 +1,3 @@
 // Package operator implements logical, arithmetic, bitwise and comparison operators as functions (like the Python
-// operator module).
+// operator module). Includes unary, binary, and nary functions with overflow checked variants.
 package operator
