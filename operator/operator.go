85a5227d4c259023a6d0110850e10b777feae91e
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-14 19:51:34 +0000

    Update docstrings

--- operator/operator.go
+++ operator/operator.go
@@ -1,3 +1,10 @@
 // Package operator implements logical, arithmetic, bitwise and comparison operators as functions (like the Python
 // operator module). Includes unary, binary, and nary functions with overflow checked variants.
+//
+// Home page https://tawesoft.co.uk/go
+//
+// For source code see https://github.com/tawesoft/go/tree/master/operator
+//
+// For documentation see https://godoc.org/tawesoft.co.uk/go/operator
+//
 package operator
