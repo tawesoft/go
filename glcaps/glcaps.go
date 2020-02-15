293b3f05246e10f6eb44651f37e0f6593235433c
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-15 03:50:06 +0000

    Restore docstring

---
+++ glcaps/glcaps.go
@@ -0,0 +1,3 @@
+// Package glcaps provides a nice interface to declare OpenGL capabilities you care about, including minimum required
+// extensions or capabilities. Glcaps has no dependencies and is agnostic to the exact OpenGL binding used.
+package glcaps
