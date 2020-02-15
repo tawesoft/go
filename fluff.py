202169064286c1a2dad1e7031a1c2ad519c4ee24
Ben Sarah Golightly <golightly.ben@googlemail.com>
2020-02-15 21:48:23 +0000

    Update branded fluff

--- fluff.py
+++ fluff.py
@@ -15,6 +15,7 @@
 # warranty.
 
 
+import datetime
 import textwrap
 from dataclasses import dataclass, field
 from typing import List
@@ -537,6 +538,191 @@ import tawesoft.co.uk/go/{module.id}
             fp.write(content)
 
 
+def make_base_html():
+    template="""
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Tawesoft Go monorepo</title>
+    <meta name="description" content="A monorepo for small Go (golang) modules maintained by Tawesoft®" />
+    <meta name="go-import" content="tawesoft.co.uk/go git https://github.com/tawesoft/go">
+    <style>
+        html, body                   {{ color: #323232; background-color: #FFF; font-family: sans-serif; padding: 0; margin: 0; }}
+        body                         {{ min-width: 800px; padding: 1em; }}
+        h2, h3                       {{ margin-top: 2em; }}
+        a                            {{ color: #b63355; text-decoration: none; }}
+        table                        {{ width: 100%; border-spacing: 0 0; }}
+        table, thead, tbody, tr      {{ border: 0; border-collapse: separate; text-align: left; }}
+        th                           {{ border-bottom: 1px solid #000; }}
+        th, td                       {{ padding: 0.5em; }}
+        li                           {{ margin: 1em 0; }}
+        
+        #container                   {{ max-width: 800px; margin: 0 auto; }}
+        #footer                      {{ max-width: 1200px; margin: 0 auto; text-align: right; margin-top: 2em; }}
+        #footer li                   {{ margin: 0; }}
+        
+        table.pkg td.stable          {{ text-align: center; }}
+        table.pkg td.stable > span   {{ color: #b63355; }}
+        table.pkg td.links           {{ padding-left: 0; }}
+        table.pkg td.links > a       {{ display: inline-block; background-color: #FEE; padding: 0.5em 1em; margin: 0.25em; }}
+        table.pkg td.links > a:first-child {{ margin-left: 0; }}
+        table.pkg td.links > a:hover {{ background-color: #b63355; color: #FFF; }}
+        table.pkg tbody > tr         {{ height: 4em; line-height: 1.5em; }}
+        
+div#footer {{
+  padding-top: 64px;
+  color: #686062; }}
+  div#footer > div.block {{
+    text-align: right; }}
+  div#footer a {{
+    color: #b73154; }}
+  div#footer a:hover {{
+    text-decoration: underline; }}
+  div#footer ul {{
+    list-style-type: none; }}
+  div#footer div.smedia {{
+    float: left;
+    margin-bottom: 1em; }}
+    div#footer div.smedia a {{
+      display: inline-block;
+      width: 64px;
+      height: 64px;
+      margin-right: 16px;
+      background-image: url("smedia.png");
+      background-repeat: no-repeat; }}
+      div#footer div.smedia a:hover {{
+        text-decoration: none !important; }}
+      div#footer div.smedia a.twitter {{
+        width: 42px;
+        background-position: 0px 14px; }}
+        div#footer div.smedia a.twitter:hover {{
+          background-position: -43px 14px; }}
+      div#footer div.smedia a.linkedin {{
+        background-position: -86px 0; }}
+        div#footer div.smedia a.linkedin:hover {{
+          background-position: -150px 0; }}
+      div#footer div.smedia a.facebook {{
+        background-position: -86px -64px; }}
+        div#footer div.smedia a.facebook:hover {{
+          background-position: -150px -64px; }}
+      div#footer div.smedia a.github {{
+        background-position: -86px -128px; }}
+        div#footer div.smedia a.github:hover {{
+          background-position: -150px -128px; }}
+    </style>
+</head>
+<body>
+
+<div id="container">
+
+<div style="float: right; margin: 1em;">
+<a href="https://www.tawesoft.co.uk/products/open-source-software"><img src="https://www.tawesoft.co.uk/media/0/logo-240r.png" alt="Tawesoft Logo" /></a>
+</div>
+
+<h1>tawesoft.co.uk/go</h1>
+
+<p>A monorepo for small Go modules maintained by <a href="https://www.tawesoft.co.uk/">Tawesoft<sup>&reg;</sup></a></p>
+
+<p>This is permissively-licensed open source software but exact licenses may vary between modules.</p>
+
+<h2>Download</h2>
+
+<pre>go get -u tawesoft.co.uk/go</pre>
+
+<h2>Packages</h2>
+
+<table class="pkg">
+    <thead>
+        <tr>
+            <th style="width: 50%;">Module</th>
+            <th style="width: 20%;">Links</th>
+            <th style="width: 10%;">Stable?</th>
+            <th style="width: 20%;">License</th>
+        </tr>
+    </thead>
+    <tbody>
+{modules}
+    </tbody>
+</table>
+
+<h2>Links:</h2>
+
+<ul>
+    <li>Home: <a href="https://tawesoft.co.uk/go">tawesoft.co.uk/go</a></li>
+    <li>Docs hub: <a href="https://godoc.org/tawesoft.co.uk/go">godoc.org/tawesoft.co.uk/go</a></li>
+    <li>Repository: <a href="https://github.com/tawesoft/go">github.com/tawesoft/go</a></li>
+    <li>Or <a href="https://pkg.go.dev/search?q=tawesoft">search "tawesoft"</a> on <a href="https://go.dev/">go.dev</a></li>
+</ul>
+
+<h2>Support:</h2>
+
+<h3>Free and Community Support</h3>
+
+<ul>
+    <li><a href="https://github.com/tawesoft/go/issues">GitHub issues</a></li>
+    <li>Email <a href="mailto:open-source@tawesoft.co.uk">open-source@tawesoft.co.uk</a> (feedback welcomed, but support is "best effort")</li>
+</ul>
+
+<h3>Commercial Support</h3>
+
+<p>Open source software from Tawesoft® backed by commercial support options.</p>
+
+<p>Email <a href="mailto:open-source@tawesoft.co.uk">open-source@tawesoft.co.uk</a>  or visit
+<a href="https://www.tawesoft.co.uk/products/open-source-software">tawesoft.co.uk/products/open-source-software</a> to learn more.</p>
+
+</div>
+
+<div id="footer" class="toplevel block">
+    <div class="smedia"
+        ><a rel="external nofollow" class="facebook" href="https://www.facebook.com/tawesoft"         title="Find Tawesoft on Facebook">&nbsp;</a
+        ><a rel="external nofollow" class="linkedin" href="https://www.linkedin.com/company/10149194" title="Connect with Tawesoft on LinkedIn">&nbsp;</a
+        ><a rel="external nofollow" class="github"   href="https://github.com/tawesoft"               title="Code with Tawesoft on GitHub">&nbsp;</a
+        ><a rel="external nofollow" class="twitter"  href="https://twitter.com/tawesoft"              title="Follow @tawesoft on Twitter">&nbsp;</a
+    ></div>
+    <div class="block">
+        <p lang="en-gb" class="en">Tawesoft &reg; is a trading style and registered trade mark of <strong>Tawesoft Ltd</strong><br />
+        Registered in England and Wales, Company Number 9735741</p>
+        <p lang="cy" class="cy">Tawesoft &reg; yn enw masnach a nod masnach cofrestredig o <strong>Tawesoft Cyf</strong><br />
+        Cofrestrwyd yng Nghymru a Lloegr, Rhif Cwmni 9735741</p>
+        <p>Copyright / <span lang="cy">Hawlfraint</span> &copy; <a href="https://www.tawesoft.co.uk/">Tawesoft Ltd</a> 2019 - 2020</p>
+        
+        <ul>
+            <li><a href="https://www.tawesoft.co.uk/about/tawesoft">About Tawesoft / <span lang="cy">Pwy yw Tawesoft?</span></a></li>
+            <li><a href="https://www.tawesoft.co.uk/about/contact">Contact us / <span lang="cy">Cysylltu â ni</span></a></li>
+            <li><a href="https://www.tawesoft.co.uk/about/privacy">Privacy / <span lang="cy">Preifatrwydd</span></a></li>
+        </ul>
+    </div>
+</div>
+
+</body>
+</html>
+""".strip()
+
+    row="""
+        <tr>
+            <td><b>{module.id}</b><br />{module.desc.short}<br /></td>
+            <td class="links">
+                <a href="https://godoc.org/tawesoft.co.uk/go/{module.id}">docs</a>
+                <a href="https://github.com/tawesoft/go/tree/master/{module.id}">src</a>
+            </td>
+            <td class="stable">{stable}</td>
+            <td>
+                <a href="https://github.com/tawesoft/go/tree/master/{module.id}/COPYING.md">{module.license.id}</a>
+            </td>
+        </tr>
+""".strip()
+
+    modules="\n".join([row.format(
+        module=i,
+        stable="<span>✔</span> yes" if i.stable else "<span>✘</span> <b>no</b>"
+    ) for i in catalog])
+
+    with open("go.html", "w") as fp:
+        fp.write(template.format(modules=modules))
+
+
+
 def run(fn):
     print("> %s: %s" % (fn.__name__, fn.__doc__))
     fn()
@@ -547,6 +733,7 @@ def run_many(*args):
         run(i)
 
 run_many(
+    make_base_html,
     make_base_go,
     make_module_go,
     make_base_license_txt,
